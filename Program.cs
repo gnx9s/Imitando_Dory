a8a2081db4b62408c7151d99b461f7d19dcfba2a
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-01 04:16:15 -0300

    exercício feito com sucesso.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string frase;
+string frase_em_baleiês;
+
+
+Console.WriteLine("Escreva uma frase a ser traduzida: ");
+frase = Console.ReadLine()!;
+
+frase_em_baleiês = frase
+    .Replace("a", "aaa")
+    .Replace("A", "AAA")
+    .Replace("e", "eee")
+    .Replace("E", "EEE")
+    .Replace("i", "iii")
+    .Replace("I", "III")
+    .Replace("o", "ooo")
+    .Replace("O", "OOO")
+    .Replace("u", "uuu")
+    .Replace("U", "UUU")
+;
+            
+Console.WriteLine($"Em baleiês: {frase_em_baleiês}");
